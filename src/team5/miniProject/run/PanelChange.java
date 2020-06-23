a4279992bfa86a045f0ecdadbd6aaa6622b893f0
JMKIM <JMKIM@DESKTOP-ENR1RV7>
2020-06-23 17:13:24 +0900

    20200623_run패키지_run클래스파일, change클래스 (김재민)

---
+++ src/team5/miniProject/run/PanelChange.java
@@ -0,0 +1,266 @@
+package miniProject.Run;
+
+import javax.swing.JFrame;
+
+import miniProject.JPanel_Login;
+import miniProject.JPanel_Main;
+import miniProject.PanelList;
+import miniProject.chapter.chap01.Chap01ProgramPart01;
+import miniProject.chapter.chap01.Chap01ProgramPart02;
+import miniProject.chapter.chap01.Chap01ProgramPart03;
+import miniProject.chapter.chap01.Chap01ProgramPart04;
+import miniProject.chapter.chap01.Chap01ProgramPart05;
+import miniProject.chapter.chap01.Chap01ProgramPart06;
+import miniProject.chapter.chap01.Chap01ProgramPart07;
+import miniProject.chapter.chap03.Chap03OperatorPart01;
+import miniProject.chapter.chap03.Chap03OperatorPart02;
+import miniProject.chapter.chap03.Chap03OperatorPart03;
+import miniProject.chapter.chap03.Chap03OperatorPart04;
+import miniProject.chapter.chap03.Chap03OperatorPart05;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuiz1;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuiz2;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuizAnswer1;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuizAnswer2;
+import miniProject.chapter.chap03.test.Chap03OperatorTest1;
+import miniProject.chapter.chap03.test.Chap03OperatorTest2;
+import miniProject.chapter.chap03.test.Chap03OperatorTestAnswer1;
+import miniProject.chapter.chap05.Chap05Array;
+import miniProject.chapter.chap05.Chap05ArrayPart1;
+import miniProject.chapter.chap05.Chap05ArrayPart2;
+import miniProject.chapter.chap05.Chap05ArrayPart3;
+import miniProject.chapter.chap05.Chap05ArrayPart4;
+import miniProject.chapter.chap05.Chap05ArrayPart5;
+import miniProject.chapter.chap05.Chap05ArrayPart6;
+import miniProject.chapter.chap06.Chap06;
+import miniProject.chapter.chap06.Chap06Part1NLoop1;
+import miniProject.chapter.chap06.Chap06Part1NLoop2;
+import miniProject.chapter.chap06.Chap06Part1NLoop3;
+import miniProject.chapter.chap06.Chap06Part1NLoop4;
+import miniProject.chapter.chap06.Chap06Part1NLoop5;
+import miniProject.chapter.chap06.Chap06Part2DArray1;
+import miniProject.chapter.chap06.Chap06Part2DArray2;
+import miniProject.chapter.chap06.Chap06Part2DArray3;
+import miniProject.chapter.chap06.Chap06Part2DArray4;
+
+public class PanelChange extends JFrame{
+
+	public PanelList panellist =null;
+	public Chap03OperatorPart01 operator_part1 = null;
+	public Chap03OperatorPart02 operator_part2 = null;
+	public Chap03OperatorPart03 operator_part3 = null;
+	public Chap03OperatorPart04 operator_part4 = null;
+	public Chap03OperatorPart05 operator_part5 = null;
+	public Chap03OperatorQuiz1 operator_quiz1 = null;
+	public Chap03OperatorQuiz2 operator_quiz2 = null;
+	public Chap03OperatorTest1 operator_test1 = null;
+	public Chap03OperatorTest2 operator_test2 = null;
+	public Chap03OperatorQuizAnswer1 operator_quizAnswer1 = null;
+	public Chap03OperatorQuizAnswer2 operator_quizAnswer2 = null;
+	public Chap03OperatorTestAnswer1 operator_testAnswer1 = null;
+	public JPanel_Login jpanel_login = null;
+	public JPanel_Main jpanel_main = null;
+	public Chap01ProgramPart01 program_part1 = null;
+	public Chap01ProgramPart02 program_part2 = null;
+	public Chap01ProgramPart03 program_part3 = null;
+	public Chap01ProgramPart04 program_part4 = null;
+	public Chap01ProgramPart05 program_part5 = null;
+	public Chap01ProgramPart06 program_part6 = null;
+	public Chap01ProgramPart07 program_part7 = null;
+	public Chap05Array array_main = null;
+	public Chap05ArrayPart1 array_part1 = null;
+	public Chap05ArrayPart2 array_part2 = null;
+	public Chap05ArrayPart3 array_part3 = null;
+	public Chap05ArrayPart4 array_part4 = null;
+	public Chap05ArrayPart5 array_part5 = null;
+	public Chap05ArrayPart6 array_part6 = null;
+	public Chap06  nloop_main= null;
+	public Chap06Part1NLoop1  nloop_part1= null;
+	public Chap06Part1NLoop2  nloop_part2= null;
+	public Chap06Part1NLoop3  nloop_part3= null;
+	public Chap06Part1NLoop4  nloop_part4= null;
+	public Chap06Part1NLoop5  nloop_part5= null;
+	public Chap06Part2DArray1 darray_part1= null;
+	public Chap06Part2DArray2 darray_part2= null;
+	public Chap06Part2DArray3 darray_part3= null;
+	public Chap06Part2DArray4 darray_part4= null;
+	
+	
+	
+	
+	
+public void change(String panelName){
+		
+		if(panelName.equals("operator_part1")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_part1);
+			revalidate();
+			repaint();
+		}
+		else if(panelName.equals("operator_part2")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_part2);
+			revalidate();
+			repaint();
+		}
+		else if(panelName.equals("operator_part3")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_part3);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_part4")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_part4);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_part5")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_part5);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_quiz1")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_quiz1);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_quiz2")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_quiz2);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_test1")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_test1);
+			revalidate();
+			repaint();
+		}
+		else if(panelName.equals("operator_test2")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_test2);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_quizAnswer1")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_quizAnswer1);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_quizAnswer2")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_quizAnswer2);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("operator_testAnswer1")){
+			getContentPane().removeAll();
+			getContentPane().add(operator_testAnswer1);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("panellist")){
+			getContentPane().removeAll();
+			getContentPane().add(panellist);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("jpanel_main")){
+			getContentPane().removeAll();
+			getContentPane().add(jpanel_main);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("jpanel_login")){
+			getContentPane().removeAll();
+			getContentPane().add(jpanel_login);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("program_part1")){
+			getContentPane().removeAll();
+			getContentPane().add(program_part1);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("program_part2")){
+			getContentPane().removeAll();
+			getContentPane().add(program_part2);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("program_part3")){
+			getContentPane().removeAll();
+			getContentPane().add(program_part3);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("program_part4")){
+			getContentPane().removeAll();
+			getContentPane().add(program_part4);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("program_part5")){
+			getContentPane().removeAll();
+			getContentPane().add(program_part5);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("program_part6")){
+			getContentPane().removeAll();
+			getContentPane().add(program_part6);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("program_part7")){
+			getContentPane().removeAll();
+			getContentPane().add(program_part7);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("array_main")){
+			getContentPane().removeAll();
+			getContentPane().add(array_main);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("array_part1")){
+			getContentPane().removeAll();
+			getContentPane().add(array_part1);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("array_part2")){
+			getContentPane().removeAll();
+			getContentPane().add(array_part2);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("array_part3")){
+			getContentPane().removeAll();
+			getContentPane().add(array_part3);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("nloop_main")){
+			getContentPane().removeAll();
+			getContentPane().add(nloop_main);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("nloop_part1")){
+			getContentPane().removeAll();
+			getContentPane().add(nloop_part1);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("nloop_part2")){
+			getContentPane().removeAll();
+			getContentPane().add(nloop_part2);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("nloop_part3")){
+			getContentPane().removeAll();
+			getContentPane().add(nloop_part3);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("nloop_part4")){
+			getContentPane().removeAll();
+			getContentPane().add(nloop_part4);
+			revalidate();
+			repaint();
+		}else if(panelName.equals("nloop_part5")){
+			getContentPane().removeAll();
+			getContentPane().add(nloop_part5);
+			revalidate();
+			repaint();
+		}
+		
+		
+		
+		
+		
+
+		
+	}
+	
+}
---
+++ src/team5/miniProject/run/run.java
@@ -0,0 +1,91 @@
+package miniProject.Run;
+
+import javax.swing.JFrame;
+
+import miniProject.JPanel_Login;
+import miniProject.JPanel_Main;
+import miniProject.PanelList;
+import miniProject.chapter.chap01.Chap01ProgramPart01;
+import miniProject.chapter.chap01.Chap01ProgramPart02;
+import miniProject.chapter.chap01.Chap01ProgramPart03;
+import miniProject.chapter.chap01.Chap01ProgramPart04;
+import miniProject.chapter.chap01.Chap01ProgramPart05;
+import miniProject.chapter.chap01.Chap01ProgramPart06;
+import miniProject.chapter.chap01.Chap01ProgramPart07;
+import miniProject.chapter.chap03.Chap03OperatorPart01;
+import miniProject.chapter.chap03.Chap03OperatorPart02;
+import miniProject.chapter.chap03.Chap03OperatorPart03;
+import miniProject.chapter.chap03.Chap03OperatorPart04;
+import miniProject.chapter.chap03.Chap03OperatorPart05;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuiz1;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuiz2;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuizAnswer1;
+import miniProject.chapter.chap03.quiz.Chap03OperatorQuizAnswer2;
+import miniProject.chapter.chap03.test.Chap03OperatorTest1;
+import miniProject.chapter.chap03.test.Chap03OperatorTest2;
+import miniProject.chapter.chap03.test.Chap03OperatorTestAnswer1;
+import miniProject.chapter.chap05.Chap05Array;
+import miniProject.chapter.chap05.Chap05ArrayPart1;
+import miniProject.chapter.chap05.Chap05ArrayPart2;
+import miniProject.chapter.chap05.Chap05ArrayPart3;
+import miniProject.chapter.chap05.Chap05ArrayPart4;
+import miniProject.chapter.chap05.Chap05ArrayPart5;
+import miniProject.chapter.chap05.Chap05ArrayPart6;
+import miniProject.chapter.chap06.Chap06;
+import miniProject.chapter.chap06.Chap06Part1NLoop1;
+import miniProject.chapter.chap06.Chap06Part1NLoop2;
+import miniProject.chapter.chap06.Chap06Part1NLoop3;
+import miniProject.chapter.chap06.Chap06Part1NLoop4;
+import miniProject.chapter.chap06.Chap06Part1NLoop5;
+
+public class run {
+	
+	public static void main(String[] args) {
+		PanelChange win = new PanelChange();
+	
+	win.setTitle("Operator");
+	win.panellist = new PanelList(win);
+	win.operator_part1 = new Chap03OperatorPart01(win);
+	win.operator_part2 = new Chap03OperatorPart02(win);
+	win.operator_part3 = new Chap03OperatorPart03(win);
+	win.operator_part4 = new Chap03OperatorPart04(win);
+	win.operator_part5 = new Chap03OperatorPart05(win);
+	win.operator_quiz1 = new Chap03OperatorQuiz1(win);
+	win.operator_quiz2 = new Chap03OperatorQuiz2(win);
+	win.operator_test1 = new Chap03OperatorTest1(win);
+	win.operator_test2 = new Chap03OperatorTest2(win);
+	win.operator_quizAnswer1 = new Chap03OperatorQuizAnswer1(win);
+	win.operator_quizAnswer2 = new Chap03OperatorQuizAnswer2(win);
+	win.operator_testAnswer1 = new Chap03OperatorTestAnswer1(win);
+	win.jpanel_login = new JPanel_Login(win);
+	win.jpanel_main = new JPanel_Main(win);
+	win.program_part1 = new Chap01ProgramPart01(win);
+	win.program_part2 = new Chap01ProgramPart02(win);
+	win.program_part3 = new Chap01ProgramPart03(win);
+	win.program_part4 = new Chap01ProgramPart04(win);
+	win.program_part5 = new Chap01ProgramPart05(win);
+	win.program_part6 = new Chap01ProgramPart06(win);
+	win.program_part7 = new Chap01ProgramPart07(win);
+	win.array_main = new  Chap05Array(win); 
+	win.array_part1 = new  Chap05ArrayPart1(win);
+	win.array_part2 = new  Chap05ArrayPart2(win);
+	win.array_part3 = new  Chap05ArrayPart3(win);
+	win.array_part4 = new  Chap05ArrayPart4(win);
+	win.array_part5 = new  Chap05ArrayPart5(win);
+	win.array_part6 = new  Chap05ArrayPart6(win);
+	win.nloop_main = new Chap06(win);
+	win.nloop_part1 = new Chap06Part1NLoop1(win);
+	win.nloop_part2 = new Chap06Part1NLoop2(win);
+	win.nloop_part3 = new Chap06Part1NLoop3(win);
+	win.nloop_part4 = new Chap06Part1NLoop4(win);
+	win.nloop_part5 = new Chap06Part1NLoop5(win);
+	
+	
+	
+	win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	win.setSize(380,680);
+	win.setVisible(true);
+	win.add(win.jpanel_main);
+
+	}
+}
